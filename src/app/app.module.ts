33662713085eeab6944496077795134fbdf46bb6
Fatin Nabila <fatin@seetrustudio.com>
2019-03-08 14:41:42 +0800

    1. Add history component (Work In Progress)
    2. Add routing to navigate between history and calculation page
    3. Add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,36 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 import { calculatorComponent } from './calculator/calculator.component';
+import { HistoryComponent } from './history/history.component';
+import { Routes , RouterModule} from '@angular/router'
+
+const appRoute : Routes  = [
+
+  { path : '' , component : calculatorComponent},
+  { path : 'history' , component : HistoryComponent}
+
+]
 
 @NgModule({
   imports:      [ 
     BrowserModule, 
     FormsModule ,
+    RouterModule.forRoot(appRoute)
   ],
 
   declarations: [ 
     AppComponent,
     HelloComponent,
-    calculatorComponent
+    calculatorComponent,
+    HistoryComponent
   ],
 
   bootstrap: [ 
     AppComponent 
+  ],
+
+  providers :[
+  //  {provide: APP_BASE_HREF, useValue: '/'}
   ]
 })
 export class AppModule { }
